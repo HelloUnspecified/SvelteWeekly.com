bcb13ecf4d0117fcd56a597d579adbb11ae53176
Clark Sell <clark.sell@csell.net>
2022-02-18 17:16:59 -0600

    fix: now compiles

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,13 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 import { resolve } from 'path';
-import { mdsvex } from 'mdsvex';
-import mdsvexConfig from './mdsvex.config.js';
 
-//todo this needs to get checked on the build rigs
 const dev = process.env.NODE_ENV === 'development' ? true : false;
 console.log('process.env.NODE_ENV', dev);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
+	extensions: ['.svelte'],
 	kit: {
 		trailingSlash: 'never',
 		serviceWorker: {
@@ -36,14 +33,12 @@ const config = {
 				preprocess({
 					postcss: true,
 					preserve: ['ld+json']
-				}),
-				mdsvex(mdsvexConfig)
+				})
 		  ]
 		: [
 				preprocess({
 					preserve: ['ld+json']
-				}),
-				mdsvex(mdsvexConfig)
+				})
 		  ]
 };
 
